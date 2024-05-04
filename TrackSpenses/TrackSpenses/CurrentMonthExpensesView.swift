395b47dffff97f5661b5789d36679425919445e0
StanciuOctav <octav.stanciu00@gmail.com>
2024-05-04 21:25:19 +0300

    Added the TabView and created the tabItem views

---
+++ TrackSpenses/TrackSpenses/CurrentMonthExpensesView.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentMonthExpensesView.swift
+//  TrackSpenses
+//
+//  Created by Octav Stanciu on 03.05.2024.
+//
+
+import Foundation
---
+++ TrackSpenses/TrackSpenses/HistoryView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  TrackSpenses
+//
+//  Created by Octav Stanciu on 03.05.2024.
+//
+
+import SwiftUI
+
+/// This view should display a list with all the expenses added in the app, some sort of history
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
